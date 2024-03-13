a80786edd61fa02ea772ea51fd43988516b97acf
nika <jamaspishvilinika@icloud.com>
2024-03-13 15:20:25 +0400

    changed some styles to make it look better on mobile

--- client/src/pages/404/404.jsx
+++ client/src/pages/404/404.jsx
@@ -5,7 +5,7 @@ function NotFound() {
   return  <div className='NotFoundDiv'>
   <h1>Sorry, this page isn't available.</h1>
 
-  <p>The link you followed may be broken, or the page may have been removed. <a href="http://localhost:5173/home">Go back to Lumina</a></p>
+  <p>The link you followed may be broken, or the page may have been removed. <a href="https://luminawow.netlify.app/home">Go back to Lumina</a></p>
   </div>
 
 }
