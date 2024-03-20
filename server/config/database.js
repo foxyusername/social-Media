0bdacf0a35eb6e50fd53f33e03bdcdcc7d9e050d
nika <jamaspishvilinika@icloud.com>
2024-03-20 14:25:04 +0400

    migrating to new database

--- server/config/database.js
+++ server/config/database.js
@@ -7,7 +7,13 @@ const pool=mysql.createConnection({
     host:process.env.HOST,
     user:process.env.USER,
     password:process.env.PASSWORD,
-    database:process.env.DATABASE
+    database:process.env.DATABASE,
+    port:process.env.PORTDATABASE,
+    ssl:{
+        mode:'REQUIRED',
+        ca: process.env.CA,
+        rejectUnauthorized: false 
+    }
 })
 
 export default pool
